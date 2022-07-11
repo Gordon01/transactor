f6c38fb64971a47845cfe84f1f3d4a2d5de62581
Alexander <etc9053@gmail.com>
2022-07-12 02:56:34 +0400

    Add unit-tests

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn main() {
 }
 
 // We accept any type of reader, so a file can be easily replaced with TCP stream as said in the problem description.
-fn process(reader: impl io::Read, writer: impl io::Write) -> Result<(), Box<dyn Error>> {
+pub fn process(reader: impl io::Read, writer: impl io::Write) -> Result<(), Box<dyn Error>> {
     // «Whitespaces and decimal precisions (up to four places past the decimal) must be accepted by your program.»
     let mut rdr = ReaderBuilder::new().trim(Trim::All).from_reader(reader);
 
@@ -63,4 +63,122 @@ fn process(reader: impl io::Read, writer: impl io::Write) -> Result<(), Box<dyn 
     wtr.flush()?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    // We use HashMap iterator for tests, so we need to sort output for consistency.
+    fn sort_output_by_client_id(string: &mut String) {
+        let mut lines: Vec<_> = string.lines().collect();
+        let slice = &mut lines[1..];
+        slice.sort_by(|a, b| {
+            let a_id: u16 = a.split(",").next().unwrap().parse().unwrap();
+            let b_id: u16 = b.split(",").next().unwrap().parse().unwrap();
+            a_id.cmp(&b_id)
+        });
+        *string = lines.join("\n");
+    }
+
+    fn run_process(input: &str) -> String {
+        let mut result = Vec::new();
+        super::process(input.as_bytes(), &mut result).expect("process transactions");
+        let mut data = String::from_utf8(result).expect("convert result to string");
+        sort_output_by_client_id(&mut data);
+
+        data
+    }
+
+    #[test]
+    fn deposits() {
+        let transactions = "\
+type,client,tx,amount
+deposit,1,1,100.00
+deposit,1,2,200.00
+deposit,2,1,300.00
+deposit,2,2,400.00";
+        let output = "\
+client,available,held,total,locked
+1,300.0,0.0,300.0,false
+2,700.0,0.0,700.0,false";
+        assert_eq!(run_process(transactions), output);
+    }
+
+    #[test]
+    fn deposits_and_withdrawals() {
+        let transactions = "\
+type,client,tx,amount
+deposit,1,1,100.00
+withdrawal,1,2,200.00
+deposit,2,1,300.00
+withdrawal,2,2,200.00";
+        let output = "\
+client,available,held,total,locked
+1,100.0,0.0,100.0,false
+2,100.0,0.0,100.0,false";
+        assert_eq!(run_process(transactions), output);
+    }
+
+    #[test]
+    fn dispute() {
+        let transactions = "\
+type,client,tx,amount
+deposit,1,1,100.00
+dispute,1,1,0.00";
+        let output = "\
+client,available,held,total,locked
+1,0.0,100.0,100.0,false";
+        assert_eq!(run_process(transactions), output);
+    }
+
+    #[test]
+    fn dispute_no_id() {
+        let transactions = "\
+type,client,tx,amount
+deposit,1,1,100.00
+dispute,1,2,0.00";
+        let output = "\
+client,available,held,total,locked
+1,100.0,0.0,100.0,false";
+        assert_eq!(run_process(transactions), output);
+    }
+
+    #[test]
+    fn dispute_and_resolve() {
+        let transactions = "\
+type,client,tx,amount
+deposit,1,1,100.00
+dispute,1,1,0.00
+resolve,1,1,0.00";
+        let output = "\
+client,available,held,total,locked
+1,100.0,0.0,100.0,false";
+        assert_eq!(run_process(transactions), output);
+    }
+
+    #[test]
+    fn dispute_and_chargeback() {
+        let transactions = "\
+type,client,tx,amount
+deposit,1,1,100.00
+deposit,1,2,200.00
+dispute,1,2,0.00
+chargeback,1,2,0.00";
+        let output = "\
+client,available,held,total,locked
+1,100.0,0.0,100.0,true";
+        assert_eq!(run_process(transactions), output);
+    }
+
+    #[test]
+    fn chargeback_wrong_state() {
+        let transactions = "\
+type,client,tx,amount
+deposit,1,1,100.00
+deposit,1,2,200.00
+chargeback,1,2,0.00";
+        let output = "\
+client,available,held,total,locked
+1,300.0,0.0,300.0,false";
+        assert_eq!(run_process(transactions), output);
+    }
 }
